f2417e923ffa839b626847535a4d17beeb7c42aa
CronoCode120 <ctaverafernandez@gmail.com>
2024-06-17 10:35:39 +0200

    added mock pressable to EventItem

--- components/EventItem.tsx
+++ components/EventItem.tsx
@@ -1,10 +1,13 @@
-import { Text, View } from "react-native"
+import { Pressable, Text, View } from "react-native"
 import type { AppEvent } from "../types.d.ts"
 
 const EventItem = ({ item: { name } }: { item: AppEvent }) => {
     return (
         <View>
             <Text>{name}</Text>
+            <Pressable>
+                <Text>Join</Text>
+            </Pressable>
         </View>
     )
 }
