784c0b6bb0a251b039dae0307266853548973c99
CronoCode120 <ctaverafernandez@gmail.com>
2024-06-17 10:14:18 +0200

    created home page component

---
+++ components/HomePage.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+import { View, Text, FlatList } from "react-native"
+
+const HomePage = () => {
+    const [events, setEvents] = useState([])
+
+
+    return (
+        <View>
+        </View>
+    )
+}
+
+export default HomePage
