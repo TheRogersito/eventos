d0dbab59c4e36713ed744063399e63219f0168fc
CronoCode120 <ctaverafernandez@gmail.com>
2024-06-18 12:55:18 +0200

    moved reusable StyledComponents to 'index' file

--- components/styles/Events.ts
+++ components/styles/Events.ts
@@ -1,17 +1,5 @@
-import { Text, View } from "react-native";
 import styled from "styled-components";
 import { Link } from "expo-router";
-
-export const Container = styled(View)`
-    margin: 10px;
-    height: 100%;
-    position: relative;
-`
-
-export const Title = styled(Text)`
-    font-size: 30px;
-    text-align: center;
-`
 
 export const NewEventLink = styled(Link)`
     position: absolute;
---
+++ components/styles/index.ts
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+import styled from 'styled-components';
+
+export const Container = styled(View)`
+    margin: 10px;
+    height: 100%;
+    position: relative;
+`
+
+export const Title = styled(Text)`
+    font-size: 30px;
+    text-align: center;
+`
