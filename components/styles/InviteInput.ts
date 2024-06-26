043279b3fa39597a98743df9ca41a3d67ee8f1c4
CronoCode120 <ctaverafernandez@gmail.com>
2024-06-26 13:58:01 +0200

    added styles to UserList component

--- components/styles/InviteInput.ts
+++ components/styles/InviteInput.ts
@@ -1,4 +1,4 @@
-import { TextInput } from "react-native";
+import { TextInput, View } from "react-native";
 import styled from "styled-components";
 
 export const Input = styled(TextInput) <{
@@ -6,8 +6,14 @@ export const Input = styled(TextInput) <{
 }>`
     border: ${props => props.hasFocus ? '2px' : '1px'} solid black;
     border-radius: 10px;
-    height: '40px';
+    height: 40px;
     padding: 10px;
     align-items: start;
     justify-content: start;
+`
+
+export const ListWrapper = styled(View)`
+    background-color: white;
+    border: 1px solid black;
+    padding: 5px;
 `
