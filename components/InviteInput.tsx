0194a86622d0f4909e71847b1af10d4851e16abc
CronoCode120 <ctaverafernandez@gmail.com>
2024-06-26 13:57:13 +0200

    applied dependency injection to InviteInput

--- components/InviteInput.tsx
+++ components/InviteInput.tsx
@@ -1,19 +1,48 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { View } from "react-native"
 import { Label } from "./styles/UserInput"
 import { Input } from "./styles/InviteInput"
+import { getUsers } from "../api/users"
+import UserList from "./UserList"
 
 type Props = {
     label: string
+    invitedUsers: any[]
+    inviteUser: (userId: string) => void
 }
 
 const InviteInput = ({
-    label
+    label,
+    invitedUsers,
+    inviteUser
 }: Props) => {
     const [value, setValue] = useState('')
     const [hasFocus, setHasFocus] = useState(false)
+    const [users, setUsers] = useState<any[]>([])
+    const [matchedUsers, setMatchedUsers] = useState<any[]>([])
 
-    const onChange = (text: string) => setValue(text)
+    useEffect(() => {
+        getUserList()
+    }, [])
+
+    const getUserList = async () => setUsers(await getUsers())
+
+    const onChange = (text: string) => {
+        setValue(text)
+
+        // if (text.length <= 3) return setMatchedUsers([])
+
+        const filteredUsers = users.filter(user => !invitedUsers.includes(user.id))
+
+        const regex = new RegExp(text, 'ig')
+        const matchedUsers = filteredUsers.filter(user => user.name.match(regex))
+        setMatchedUsers(matchedUsers)
+    }
+
+    const onInviteUser = (userId: string) => {
+        inviteUser(userId)
+        setValue('')
+    }
 
     const onFocus = () => setHasFocus(true)
     const onBlur = () => setHasFocus(false)
@@ -31,6 +60,12 @@ const InviteInput = ({
                 onFocus={onFocus}
                 onBlur={onBlur}
             />
+            {matchedUsers.length > 0 && (
+                <UserList
+                    users={matchedUsers}
+                    inviteUser={onInviteUser}
+                />
+            )}
         </View>
     )
 }
